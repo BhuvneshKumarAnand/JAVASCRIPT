7dd9a7ae2e7e902f09b09d7d968748223db5f36a
BHUVNESH KUMAR ANAND <86562438+BhuvneshKumarAnand@users.noreply.github.com>
2023-10-19 16:00:31 +0000

    coversion of datatypes

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,18 @@
+// let score=33
+let score="33abc"
+
+score=Number(score)
+
+
+console.log(score);//NaN
+console.log(typeof(score))//number
+
+let loggedIn=1
+let booleanLoggedIn=Boolean(loggedIn)
+
+console.log(typeof(booleanLoggedIn));
+console.log(booleanLoggedIn);
+
+//1 ==> true; 0 ==> false
+// "" ==> false
+// "bharat" ==> true
