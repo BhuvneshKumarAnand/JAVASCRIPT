ad5488ff3b0de1077ce7f7a6b09ee5791d76030c
BHUVNESH KUMAR ANAND <86562438+BhuvneshKumarAnand@users.noreply.github.com>
2023-10-19 15:38:29 +0000

    datatypes

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newer version
+
+
+// alert(2+1); //we are using nodejs not brower hence it will not work
+
+//code readability should be high
+
+//7 primitive data types
+
+//number
+//string
+//bigInt
+//symbol ==> unique
+// boolean ==> true/false 
+// null ==> standalone value 
+// undefined ==> 
+
+
+// 1 non-primitive data types
+// objects
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ 01_Basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 1213213;
+let accountEmail= "bharatanand874@gmail.com";
+var accountPassword= 4763547652673;
+accountCity="delhi";
+let accountState;
+
+// accountId=2;
+
+/*
+prefer not to use var dur to var being global variable
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
