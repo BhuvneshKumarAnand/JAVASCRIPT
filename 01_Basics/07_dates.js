7b67bee22dafe91eb332494ec66b36209536e823
BHUVNESH KUMAR ANAND <86562438+BhuvneshKumarAnand@users.noreply.github.com>
2023-10-25 12:53:26 +0000

    Dates in JS

---
+++ 01_Basics/07_dates.js
@@ -0,0 +1,7 @@
+//Dates in JS
+
+let myDates=new Date();
+console.log(myDates);
+console.log(myDates.toString());
+console.log(myDates.toDateString()); //print current date along with the day name
+console.log(myDates.toLocaleString()); // print date--> mm/dd/yyyy, time--> hh:mm:ss AM/PM
