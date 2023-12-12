1cec2d78d9c70021f66bb596c85567e8b136c398
BHUVNESH KUMAR ANAND <86562438+BhuvneshKumarAnand@users.noreply.github.com>
2023-12-12 09:21:47 +0000

    iteration across the array

---
+++ High Order Array loops in JS/two.js
@@ -0,0 +1,18 @@
+const myObj={
+    js:'javascript',
+    py:'python',
+    java:'java',
+    cpp:'c++'
+}
+
+for (const key in myObj) {
+    // console.log(key);
+    // console.log(`${key} :- ${myObj[key]}`);
+}
+
+const programming=['js','cpp','java','rb','py'];
+
+for (const key in programming) {
+    // console.log(key);
+    console.log(programming[key]);
+}
