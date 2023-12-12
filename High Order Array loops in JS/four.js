6d8d4bd0d281b87e38c76d67509f2d7c52ad1b8c
BHUVNESH KUMAR ANAND <86562438+BhuvneshKumarAnand@users.noreply.github.com>
2023-12-12 09:48:20 +0000

    iteration

---
+++ High Order Array loops in JS/four.js
@@ -0,0 +1,18 @@
+const myCoding=[
+    {
+        languageName:'JavaScript',
+        fileName:'js'
+    },{
+        languageName:'Python',
+        fileName:'py'
+    },{
+        languageName:'JAVA',
+        fileName:'java'
+    },{
+        languageName:'C++',
+        fileName:'cpp'
+    }
+];
+
+myCoding.forEach((item)=>console.log(item.languageName));
+myCoding.forEach((item)=>console.log(item.fileName));
